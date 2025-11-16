4fe19283d296d807d8b2f1cfc2ca3b1af27f84e5
Alejandro Rivero <alejandro.rivero@origin.cw>
2025-11-16 12:33:26 -0400

    f1

--- DocumentValidationApp/Resources/SharedResource.cs
+++
@@ -1,9 +0,0 @@
-namespace DocumentValidationApp.Resources
-{
-    public class SharedResource
-    {
-        // This class is intentionally empty.
-        // It serves as the anchor for the resource files (SharedResource.resx, SharedResource.es.resx, etc.)
-        // which are used for localization in the Blazor application.
-    }
-}
